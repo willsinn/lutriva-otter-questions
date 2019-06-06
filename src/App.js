f2c8a47b2b74d7fe0557967421a98de50da68119
William Sinn <w.h.sinn@gmail.com>
2019-06-05 23:12:57 -0400

    rename data, render data to console, base commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import otterQuestions from "./Data/otterQuestions.js";
+import questionsData from "./Data/questionsData.js";
 import './App.css';
 
 class App extends Component {
   render()  {
+    console.log('%c App-LOG | questions data ', 'color: green; font-weight: bold;', questionsData.gameInfo)
+
   return (
       <div className="App">
+        
         App is loaded with otter question data!
       </div>
     );
